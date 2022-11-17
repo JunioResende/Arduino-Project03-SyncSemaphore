4235a06d51be86da91f5c9f7cf40b8d398a7ace5
JunioResende <junioresende23@gmail.com>
2022-11-17 19:54:11 -0300

    finish

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,38 @@
 #include <Arduino.h>
 
-byte pinSemGreen1 = 8;
-byte pinSemBlue1 = 9;
-byte pinSemRed1 = 10;
-
-byte pinSemGreen2 = 5;
-byte pinSemBlue2 = 6;
-byte pinSemRed2 = 7;
-
 void setup()
 {
-  pinMode(pinSemRed2, OUTPUT);
+  // definindo os pinos digitais 5, 6, 7, 8, 9 e 10
+  // como pinos de saída.
+  pinMode(10, OUTPUT);
+  pinMode(9, OUTPUT);
+  pinMode(8, OUTPUT);
+  pinMode(7, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(5, OUTPUT);
 }
 
+// o método loop é executado repetidamente enquanto o
+// arduino estiver ligado.
 void loop()
 {
-  digitalWrite(pinSemRed2, HIGH);
+  digitalWrite(5, LOW);  // apaga o led vermelho (sinal 1)
+  digitalWrite(7, HIGH); // acende o led verde (sinal 1)
+  digitalWrite(8, HIGH); // acende o led vermelho (sinal 2)
+  delay(4000);           // espera 4 segundos
+  digitalWrite(7, LOW);  // apaga o led verde (sinal 1)
+
+  digitalWrite(6, HIGH); // acende o led amarelo (sinal 1)
+  delay(2000);           // espera 2 segundos
+  digitalWrite(6, LOW);  // apaga o led amarelo (sinal 1)
+
+  digitalWrite(5, HIGH);  // acende o led vermelho (sinal 1)
+  digitalWrite(8, LOW);   // apaga o led vermelho (sinal 2)
+  digitalWrite(10, HIGH); // acende o led verde (sinal 2)
+  delay(4000);            // espera 4 segundos
+  digitalWrite(10, LOW);  // apaga o led verde (sinal 2)
+
+  digitalWrite(9, HIGH); // acende o led amarelo (sinal 2)
+  delay(2000);           // espera 2 segundos
+  digitalWrite(9, LOW);  // apaga o led amarelo (sinal 2)
 }
