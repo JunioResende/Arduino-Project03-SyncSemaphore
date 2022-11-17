0a83d4df8cd26bc74abab8000620eaf743024121
JunioResende <junioresende23@gmail.com>
2022-11-17 18:27:15 -0300

    test pins and leds

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,19 @@
 #include <Arduino.h>
 
-void setup() {
-  // put your setup code here, to run once:
+byte pinSemGreen1 = 8;
+byte pinSemBlue1 = 9;
+byte pinSemRed1 = 10;
+
+byte pinSemGreen2 = 5;
+byte pinSemBlue2 = 6;
+byte pinSemRed2 = 7;
+
+void setup()
+{
+  pinMode(pinSemRed2, OUTPUT);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+  digitalWrite(pinSemRed2, HIGH);
 }
